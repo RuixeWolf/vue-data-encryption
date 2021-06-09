21c4ff5e92afa921196a9aad86fa1157f1f71f7d
RUIXE-LAPTOP\Ruixe <Ruixe1569898391@outlook.com>
2021-06-09 14:44:13 +0800

    完善 XHR 异常处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -13,7 +13,7 @@ const service = axios.create({
 service.interceptors.request.use(
   // onFulfilled
   request => {
-    // 添加请求 Token
+    // 添加请求 token
     const authToken = getStorageItem('authToken')
     if (authToken) {
       request.headers['Authorization'] = authToken
@@ -23,7 +23,7 @@ service.interceptors.request.use(
 
   // onRejected
   () => {
-    console.error('Error in Axios request.')
+    console.error('XHR Error in Axios Request')
     return Promise.reject('请求失败，请检查网络连接')
   }
 )
@@ -41,22 +41,36 @@ service.interceptors.response.use(
   error => {
     // ERR_INTERNET_DISCONNECTED
     if (!error.response) {
-      console.error('Error in Axios response.')
+      console.error('XHR Error: Internet Disconnected')
       return Promise.reject('请求失败，请检查网络连接')
     }
 
-    // 获取状态码与响应数据
-    const statusCode = error.response.data.statusCode
+    // 获取 HTTP 状态
+    const status = error.response.status
+    const statusText = error.response.statusText
+
+    // 处理服务器异常 HTTP 5XX
+    if (/^5\d{2}$/.test(status)) {
+      console.error(`XHR Error: ${statusText}`)
+      return Promise.reject('无法处理请求，请稍后再试')
+    }
+
+    // 获取响应数据与数据状态码（非 HTTP 状态码）
     const res = error.response.data
+    const statusCode = res.statusCode
 
     // 处理 token 异常
     if ([10000, 10001, 10002, 10003].includes(statusCode)) {
       // Token 失效，清除登录信息并转至用户登录页面
       vueStore.commit('user/SET_LOGIN_STATUS', false)
       vueRouter.replace({ name: 'userLogin' })
-      console.error('Error:', res.message)
+      console.error('XHR Error:', res.message)
       return Promise.reject('登录信息失效，请重新登录')
     }
+
+    // Default case
+    console.error('XHR Error in Axios Response')
+    return Promise.reject('请求失败，请稍后再试')
   }
 )
 
